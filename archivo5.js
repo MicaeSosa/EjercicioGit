842651c89b42277ecdfe86e83cebf67c7874d5e1
Micaela Sosa <micaesosa556@gmail.com>
2021-09-10 17:21:38 -0300

    agregue un else

--- archivo5.js
+++ archivo5.js
@@ -1,4 +1,6 @@
 let conflictos= "comprension" 
  if (conflictos!== "comprension"){
           return console.log("La violencia no es el camino, hablen")
+    }else if(conflictos==="comprension"){
+        return console.log("vamos por un fernetcito")
     }
