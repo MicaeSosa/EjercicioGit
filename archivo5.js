ea77e7bb1ca97f8fb08563b72420c9fde3249981
Dayhana <dayhanaalice99@gmail.com>
2021-09-10 17:23:09 -0300

    agregue funcion

--- archivo5.js
+++ archivo5.js
@@ -2,3 +2,9 @@ let conflictos= "comprension"
  if (conflictos!== "comprension"){
           return console.log("La violencia no es el camino, hablen")
     }
+
+    let pelea = "Crear Solucion"
+
+    function conflicto(){
+          return console.log("Los conflitos se resuelven hablando")
+    }
